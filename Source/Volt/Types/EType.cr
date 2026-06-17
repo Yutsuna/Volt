7cb5e3c4f7a6681211a091df3fd7dea3ad1e0c72
Yutsuna <leo.quinzler@epitech.eu>
2026-06-17 16:23:21 +0200

    Chore(Volt): Add EType Primitives definitions

---
+++ Source/Volt/Types/EType.cr
@@ -0,0 +1,24 @@
+module Volt
+  module Types
+
+
+    enum EType
+      Void
+      Nil
+      Bool
+      Char
+      Int8
+      Int16
+      Int32
+      Int64
+      UInt8
+      UInt16
+      UInt32
+      UInt64
+      Float32
+      Float64
+    end
+
+
+  end
+end
