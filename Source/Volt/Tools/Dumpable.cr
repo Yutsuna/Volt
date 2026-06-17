67b2ad55b7642c43b7cde2738d8a6145975403f8
Yutsuna <leo.quinzler@epitech.eu>
2026-06-17 16:23:33 +0200

    Chore(Volt): Add Base tools

---
+++ Source/Volt/Tools/Dumpable.cr
@@ -0,0 +1,27 @@
+module Krystal
+
+
+  module MDumpable
+
+    #--------------------------------------------------------------------------
+
+    def to_s( io : IO ) : Nil
+      dump(io)
+    end
+
+    #--------------------------------------------------------------------------
+
+    private macro dump( io )
+      {{io}} << self.class.name << "\n{\n"
+      {% for ivar in @type.instance_vars %}
+        {{io}} << "  " << {{ivar.name.stringify}} << ":\t  " << @{{ivar.name.id}} << '\n'
+      {% end %}
+      {{io}} << "}"
+    end
+
+    #--------------------------------------------------------------------------
+
+  end
+
+
+end
---
+++ Source/Volt/Tools/Logger.cr
@@ -0,0 +1,61 @@
+module Krystal
+
+
+  module EAnsiColor
+    RED     = "\e[31m"
+    GREEN   = "\e[32m"
+    YELLOW  = "\e[33m"
+    CYAN    = "\e[36m"
+    BLUE    = "\e[34m"
+    GREY    = "\e[90m"
+    BOLD    = "\e[1m"
+    RESET   = "\e[0m"
+  end
+
+
+  module FLog
+
+    extend self
+
+    #--------------------------------------------------------------------------
+
+    def log ( message : String, prefix : String = "Krystal", color : String = EAnsiColor::CYAN)
+      tag = "#{color}#{EAnsiColor::BOLD}[#{prefix}]#{EAnsiColor::RESET}"
+      STDOUT.puts "#{tag} #{message}"
+      STDOUT.flush
+    end
+
+    #--------------------------------------------------------------------------
+
+    def ok ( message    : String )
+      log message, "  OK  ",    EAnsiColor::GREEN
+    end
+
+    def warn ( message  : String )
+      log message, "  WARN  ",  EAnsiColor::YELLOW
+    end
+
+    def error ( message : String )
+      log message, "  ERR  ",   EAnsiColor::RED
+    end
+
+    def step ( message  : String )
+      log message, "  >>  ",    EAnsiColor::BLUE
+    end
+
+    def info ( message  : String )
+      STDOUT.puts( "#{EAnsiColor::GREY}#{message}#{EAnsiColor::RESET}" )
+      STDOUT.flush
+    end
+
+    def command ( message : String )
+      STDOUT.print( "\r#{EAnsiColor::GREY}#{message}#{EAnsiColor::RESET}" )
+      STDOUT.flush
+    end
+
+    #--------------------------------------------------------------------------
+
+  end
+
+
+end
