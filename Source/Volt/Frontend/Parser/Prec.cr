b0444ed9323532134275e3ff552c8e8418104be0
Yutsuna <leo.quinzler@epitech.eu>
2026-06-26 13:58:07 +0200

    Chore(Frontend): Update the predececence parser

--- Source/Volt/Frontend/Parser/Prec.cr
+++ Source/Volt/Frontend/Parser/Prec.cr
@@ -3,19 +3,22 @@ module Volt::Frontend
 
   enum Prec : Int32
     None       =  0
-    Assignment =  1   # =  :
+    Assignment =  1   # =  += -= etc.
     Or         =  2   # or  ||
     And        =  3   # and  &&
-    Equality   =  4   # ==  !=
-    Comparison =  5   # <  >  <=  >=
+    Equality   =  4   # ==  !=  ===  =~  !~
+    Comparison =  5   # <  >  <=  >=  <=>
     Range      =  6   # ..  ...
     Pipe       =  7   # |>
-    Term       =  8   # +  -
-    Factor     =  9   # *  /  %
-    Power      = 10   # **  (right-associative: recurse with Power - 1)
-    Unary      = 11   # !  not  -(prefix)
-    Call       = 12   # ()  []  .  ?.
-    Primary    = 13
+    BitOr      =  8   # |  ^
+    BitAnd     =  9   # &
+    Shift      = 10   # <<  >>
+    Term       = 11   # +  -  &+  &-
+    Factor     = 12   # *  /  //  %  &*
+    Power      = 13   # **  &**
+    Unary      = 14   # !  not  -(prefix)  ~  +(prefix)
+    Call       = 15   # ()  []  .  ?.
+    Primary    = 16
   end
 
 
