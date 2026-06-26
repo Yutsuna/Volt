14235285ff4ac151a164a3e0baf374098a076b60
Yutsuna <leo.quinzler@epitech.eu>
2026-06-26 09:43:09 +0200

    Chore(Frontend): Add API

--- Source/Volt/Frontend/__all__.cr
+++ Source/Volt/Frontend/__all__.cr
@@ -1,6 +1,8 @@
+require "./Types/__all__"
 require "./Lexer/__all__"
 require "./AST/__all__"
 require "./Parser/__all__"
+require "./Semantic/__all__"
 
 
 module Volt::Frontend
@@ -8,6 +10,16 @@ module Volt::Frontend
 
   def self.parse( source : String, file : String = "<unknown>" ) : Program
     Parser.new( source, file ).parse
+  end
+
+
+  def self.analyse( source : String, file : String = "<unknown>" ) : TypedProgram
+    Analyser.new( parse( source, file ) ).analyse
+  end
+
+
+  def self.analyse( program : Program ) : TypedProgram
+    Analyser.new( program ).analyse
   end
 
 
