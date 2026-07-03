cb52764e5976af1492d9a0c304e5219381eacd47
Yutsuna <leo.quinzler@epitech.eu>
2026-07-03 22:12:28 +0200

    Fix(Benchmarks): IsPrime fixes

--- Benchmarks/Primes/IsPrime.php
+++ Benchmarks/Primes/IsPrime.php
@@ -1,13 +1,13 @@
 <?php
-function is_prime( int $n ) : bool
+function is_prime(int $n): bool
 {
-    if ( n < 2 ) {
+    if ($n < 2) {
         return false;
     }
 
     $i = 2;
-    while ( $i * $i <= $n ) {
-        if ( $n % $i == 0 ) {
+    while ($i * $i <= $n) {
+        if ($n % $i == 0) {
             return false;
         }
         $i++;
@@ -15,18 +15,19 @@ function is_prime( int $n ) : bool
     return true;
 }
 
-function count_primes( int $limit ) : int
+function count_primes(int $limit): int
 {
     $count = 0;
-    for ( $i = 2; $i < $limit; $i++ ) {
-        if ( is_prime( $i ) ) {
+    for ($i = 2; $i < $limit; $i++) {
+        if (is_prime($i)) {
             $count++;
         }
     }
     return $count;
 }
 
-limit = 100_000;
-count = count_primes( limit );
+$limit = 100_000;
+$count = count_primes($limit);
+
 echo "Number of primes up to $limit: $count\n";
 ?>
