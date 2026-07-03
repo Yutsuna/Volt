0a9a495f857bbe2a292441bc776f64c5590d16dd
Yutsuna <leo.quinzler@epitech.eu>
2026-07-03 10:23:11 +0200

    Tests(Spec): Link new samples to Tests

--- Spec/Samples/Data.cr
+++ Spec/Samples/Data.cr
@@ -12,17 +12,34 @@ module Volt::Spec
     include MStreamable
 
     EXPECTED_DATA = {
+      "01.0.return_exit" => {exit_code: 84, stderr: "", stdout: ""},
+      #------------------------------------------------------------------------------------
       "01.a.HelloWorld.vl"  =>  {exit_code: 0,  stderr: "", stdout: "Hello, Volt!\n"},
+      #------------------------------------------------------------------------------------
       "01.b.Arithmetic.vl"  =>  {exit_code: 0,  stderr: "", stdout: "PASS\n"*44},
+      #------------------------------------------------------------------------------------
       "01.c.Macro.vl"       =>  {exit_code: 0,  stderr: "", stdout: "[INFO] #{File.dirname(SAMPLES_01_DIR)}/01.c.Macro.vl (line 9) : expression is: foo\n[INFO] #{File.dirname(SAMPLES_01_DIR)}/01.c.Macro.vl (line 11) : expression is: bar\n"},
+      #------------------------------------------------------------------------------------
       "01.d.MacroControl.vl" => {exit_code: 0,  stderr: "", stdout: "LOW\nHIGH\na fruit\na vegetable\nunknown\n"},
+      #------------------------------------------------------------------------------------
       "01.e.MacroCodegen.vl" => {exit_code: 0,  stderr: "", stdout: "world\nvolt\nsection\ndone\n"},
+      #------------------------------------------------------------------------------------
       "01.f.StructNoMethods.vl" => {exit_code: 0,  stderr: "", stdout: "PASS\n"},
+      #------------------------------------------------------------------------------------
       "01.g.StructWithMethods.vl" => {exit_code: 0,  stderr: "", stdout: "PASS\n"},
+      #------------------------------------------------------------------------------------
       "01.h.ClassRaii.vl" => {exit_code: 0,  stderr: "", stdout: "ENTERING SCOPE\nCONNECTED DB 101\nINIT SESSION_MANAGER\nLEAVING SCOPE\nFINALIZE SESSION_MANAGER\nDISCONNECTED DB 101\nTEST COMPLETE\n"},
+      #------------------------------------------------------------------------------------
       "01.i.ClassInheritance.vl" => {exit_code: 0,  stderr: "", stdout: "WRITE TO USB [FlashDrive] (16384MB): kernel_backup.bin\nPRINT TO NETWORK [OfficeJet]: report.pdf\n"},
+      #------------------------------------------------------------------------------------
       "01.j.ClassMixin.vl" => {exit_code: 0,  stderr: "", stdout: "[LOG] Verifying credentials for admin\n[LOG] Executing 'rm -rf /tmp' under session SESS_999\n[LOG] Verifying credentials for guest\n[LOG] DENIED access to guest\n"},
+      #------------------------------------------------------------------------------------
+      "01.k.ClassRAIIEarlyExit.vl" => {exit_code: 0, stderr: "", stdout: "Early Return\nLOCK acquired: MainTx\nLOCK acquired: ErrorLog\nLOCK released: MainTx\nLOCK released: ErrorLog\nConstruction Crash\nLOCK acquired: NestedLock\nSimulating crash inside constructor...\nruntime error: ConstructorFailureException\n"},
+      #------------------------------------------------------------------------------------
       "01.l.ClassMixinAbstract.vl" => {exit_code: 0,  stderr: "", stdout: "Book: 'Design Patterns' by Gang of Four\nElectronic: VoltBook Pro (24 months warranty)\nBook Tax: 11.0\nLaptop Tax: 299.998\n"},
+      #------------------------------------------------------------------------------------
+      "01.m.RpgPolymorphism.vl" => {exit_code: 0, stderr: "", stdout: "Paladin Arthas HP: 120/120\nAction: Holy Shield active (Shield: 25)\nRanger Sylvanas HP: 90/90\nAction: Double Shot! (Arrows remaining: 1)\nRanger Sylvanas HP: 90/90\nAction: Double Shot! (Arrows remaining: 0)\nRanger Sylvanas HP: 90/90\nAction: Out of arrows!\nPaladin Arthas HP: 150/150\nAction: Holy Shield active (Shield: 35)\nRanger Sylvanas HP: 110/110\nAction: Out of arrows!\n"},
+      #------------------------------------------------------------------------------------
     }
 
     def initialize( file_path : String )
