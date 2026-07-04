657fbd52a68d7933b776c03ee54921efe7448a03
Yutsuna <leo.quinzler@epitech.eu>
2026-07-04 12:47:56 +0200

    Fix(Frontend): Postfix if unless before

--- Source/Volt/Frontend/Parser/Prec.cr
+++ Source/Volt/Frontend/Parser/Prec.cr
@@ -3,22 +3,23 @@ module Volt::Frontend
 
   enum Prec : Int32
     None       =  0
-    Assignment =  1   # =  += -= etc.
-    Or         =  2   # or  ||
-    And        =  3   # and  &&
-    Equality   =  4   # ==  !=  ===  =~  !~
-    Comparison =  5   # <  >  <=  >=  <=>
-    Range      =  6   # ..  ...
-    Pipe       =  7   # |>
-    BitOr      =  8   # |  ^
-    BitAnd     =  9   # &
-    Shift      = 10   # <<  >>
-    Term       = 11   # +  -  &+  &-
-    Factor     = 12   # *  /  //  %  &*
-    Power      = 13   # **  &**
-    Unary      = 14   # !  not  -(prefix)  ~  +(prefix)
-    Call       = 15   # ()  []  .  ?.
-    Primary    = 16
+    Modifier   =  1   # postfix if / unless / while / until
+    Assignment =  2   # =  += -= etc.
+    Or         =  3   # or  ||
+    And        =  4   # and  &&
+    Equality   =  5   # ==  !=  ===  =~  !~
+    Comparison =  6   # <  >  <=  >=  <=>
+    Range      =  7   # ..  ...
+    Pipe       =  8   # |>
+    BitOr      =  9   # |  ^
+    BitAnd     = 10   # &
+    Shift      = 11   # <<  >>
+    Term       = 12   # +  -  &+  &-
+    Factor     = 13   # *  /  //  %  &*
+    Power      = 14   # **  &**
+    Unary      = 15   # !  not  -(prefix)  ~  +(prefix)
+    Call       = 16   # ()  []  .  ?.
+    Primary    = 17
   end
 
 
