b9d613b664b778168f369ee58cd46d14f7e973c7
Yutsuna <leo.quinzler@epitech.eu>
2026-07-09 10:42:10 +0200

    Test(Types): Add tests for type checker polymorphism and reassignment

---
+++ Spec/Frontend/Types/TypeCheckerSpec.cr
@@ -0,0 +1,51 @@
+require "spec"
+require "../../../Source/Volt/__all__"
+
+describe "Volt::Frontend::TypeChecker - Polymorphism & Reassignment" do
+
+  it "permits reassigning a variable to different subclasses of its declared type" do
+    source = <<-VOLT
+      class Parent
+        def initialize; end
+      end
+      class ChildA < Parent
+        def initialize; end
+      end
+      class ChildB < Parent
+        def initialize; end
+      end
+
+      obj : Parent = nil
+      obj = ChildA.new()
+      obj = ChildB.new()
+    VOLT
+
+    program = Volt::Frontend.parse(source, "test_spec.vl")
+
+    expect_no_raise do
+      Volt::Frontend.analyse(program)
+    end
+  end
+
+  it "maintains the wider declared type inside conditional branches" do
+    source = <<-VOLT
+      class Parent; def initialize; end; end
+      class ChildA < Parent; def initialize; end; end
+      class ChildB < Parent; def initialize; end; end
+
+      obj : Parent = nil
+      cond = true
+      if cond
+        obj = ChildA.new()
+      else
+        obj = ChildB.new()
+      end
+    VOLT
+
+    program = Volt::Frontend.parse(source, "test_spec_branch.vl")
+    expect_no_raise do
+      Volt::Frontend.analyse(program)
+    end
+  end
+
+end
