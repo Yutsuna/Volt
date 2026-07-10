9d2abf06439a5334cf4409ba88a78fed1f0e02cb
Yutsuna <leo.quinzler@epitech.eu>
2026-07-10 11:38:18 +0200

    Tests(OpcodeAbi): Update opcode values and count to match DispatchTable

--- Spec/VM/OpcodeAbiSpec.cr
+++ Spec/VM/OpcodeAbiSpec.cr
@@ -20,24 +20,24 @@ module Volt::Spec
       Volt::IR::Opcode::CALL_NATIVE.value.should   eq 32
       Volt::IR::Opcode::RET.value.should           eq 33
       Volt::IR::Opcode::CONV_INT.value.should      eq 42
-      Volt::IR::Opcode::CALL_METHOD.value.should   eq 54
-      Volt::IR::Opcode::LOAD_GLOBAL.value.should   eq 58
-      Volt::IR::Opcode::STORE_GLOBAL.value.should  eq 59
-      Volt::IR::Opcode::NOP.value.should           eq 61
-      Volt::IR::Opcode::ADD_INT_IMM.value.should   eq 62
-      Volt::IR::Opcode::BR_LT_INT.value.should     eq 66
-      Volt::IR::Opcode::BR_NE_INT_IMM.value.should eq 77
-      Volt::IR::Opcode::LOAD_PTR.value.should      eq 78
-      Volt::IR::Opcode::STORE_PTR.value.should     eq 79
-      Volt::IR::Opcode::PTR_ADD.value.should       eq 80
-      Volt::IR::Opcode::PTR_SUB.value.should       eq 81
-      Volt::IR::Opcode::ADDR_LOCAL.value.should    eq 82
-      Volt::IR::Opcode::ADDR_FIELD.value.should    eq 83
+       Volt::IR::Opcode::CALL_METHOD.value.should   eq 51
+      Volt::IR::Opcode::LOAD_GLOBAL.value.should   eq 55
+      Volt::IR::Opcode::STORE_GLOBAL.value.should  eq 56
+      Volt::IR::Opcode::NOP.value.should           eq 57
+      Volt::IR::Opcode::ADD_INT_IMM.value.should   eq 58
+      Volt::IR::Opcode::BR_LT_INT.value.should     eq 62
+      Volt::IR::Opcode::BR_NE_INT_IMM.value.should eq 73
+      Volt::IR::Opcode::LOAD_PTR.value.should      eq 74
+      Volt::IR::Opcode::STORE_PTR.value.should     eq 75
+      Volt::IR::Opcode::PTR_ADD.value.should       eq 76
+      Volt::IR::Opcode::PTR_SUB.value.should       eq 77
+      Volt::IR::Opcode::ADDR_LOCAL.value.should    eq 78
+      Volt::IR::Opcode::ADDR_FIELD.value.should    eq 79
     end
 
-    it "keeps the opcode count at what the DispatchTable covers (0..83)" do
+    it "keeps the opcode count at what the DispatchTable covers (0..79)" do
       # Bump this and the C DispatchTable together when adding opcodes.
-      Volt::IR::Opcode.values.size.should eq 84
+      Volt::IR::Opcode.values.size.should eq 80
     end
 
     it "keeps Value::Tag ordinals stable for the C EValueTag" do
