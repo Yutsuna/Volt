635c8ab148ab97aeda43d7c81aa4a191c2a53aab
Yutsuna <leo.quinzler@epitech.eu>
2026-07-10 15:25:16 +0200

    Chore(REPL): Add missing Linux c_cc constants and configure VMIN/VTIME

--- Source/Volt/CLI/LineEditor/PosixTermios.cr
+++ Source/Volt/CLI/LineEditor/PosixTermios.cr
@@ -7,6 +7,10 @@ module Volt::CLI
     @active : Bool = false
     STDIN_FD = 0
 
+    # Linux c_cc indices, missing from Crystal's LibC bindings.
+    VTIME = 5
+    VMIN  = 6
+
     def enable_raw_mode : Nil
       return if @active
 
@@ -15,6 +19,8 @@ module Volt::CLI
 
         raw = @original
         raw.c_lflag &= ~(LibC::ECHO | LibC::ICANON | LibC::ISIG)
+        raw.c_cc[VMIN]  = 1_u8
+        raw.c_cc[VTIME] = 0_u8
 
         LibC.tcsetattr(STDIN_FD, 0, pointerof(raw))
       end
