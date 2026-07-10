3379bf5a644b31a6f717f284018bfe2002d7bf06
Yutsuna <leo.quinzler@epitech.eu>
2026-07-10 11:37:09 +0200

    Refactor(VM): Remove deprecated VM

--- Source/Volt/VM/Dispatch/Cmp.cr
+++ Source/Volt/VM/Dispatch/Cmp.cr
@@ -21,15 +21,6 @@ module Volt::VM
       when IR::Opcode::GT_F64        then IR::Value.bool( b.as_f > c.as_f )
       when IR::Opcode::GE_F64        then IR::Value.bool( b.as_f >= c.as_f )
       when IR::Opcode::CMP_INT       then IR::Value.int( ( b.as_i <=> c.as_i ).to_i64 )
-      when IR::Opcode::EQ_CASE
-        res = if b.tag == IR::Value::Tag::Regex && ( cs = volt_string( c ) )
-          b.as_regex.matches?( cs )
-        else
-          b == c
-        end
-        IR::Value.bool( res )
-      when IR::Opcode::MATCH_STR     then IR::Value.bool( c.as_regex.matches?( volt_string( b ).not_nil! ) )
-      when IR::Opcode::NOT_MATCH_STR then IR::Value.bool( !c.as_regex.matches?( volt_string( b ).not_nil! ) )
       else
         raise VoltRuntimeError.new( "unhandled comparison opcode #{op}" )
       end
