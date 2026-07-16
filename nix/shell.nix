e41fa6d655dbae52510ae2077b8d4bdd9a4f15be
Yutsuna <leo.quinzler@epitech.eu>
2026-07-16 18:23:12 +0200

    Refactor(Volt): Add nix flakes

---
+++ nix/shell.nix
@@ -0,0 +1,29 @@
+{
+  pkgs,
+  inputs,
+  ...
+}:
+
+let
+  llvmAttrs = pkgs.llvmPackages_latest;
+  voltProj = pkgs.callPackage ./package.nix { inherit inputs; };
+in
+pkgs.mkShell {
+  inputsFrom = [ voltProj ];
+
+  nativeBuildInputs = with pkgs; [
+    llvmAttrs.clang
+    llvmAttrs.lldb
+    valgrind
+    gdb
+    gtest
+    clang-tools
+    mold
+  ];
+
+  shellHook = ''
+    export CXX=clang++
+    export CC=clang
+    export NIX_CFLAGS_LINK="-fuse-ld=mold"
+  '';
+}
