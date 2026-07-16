8f7ba7ba28d80c0d25e8bf9cec1a29211cab872c
Yutsuna <leo.quinzler@epitech.eu>
2026-07-16 19:30:11 +0200

    Chore(CMake): Make bin in bin directory

--- cmake/VoltOptions.cmake
+++ cmake/VoltOptions.cmake
@@ -10,6 +10,10 @@ set( CMAKE_C_EXTENSIONS OFF )
 
 set( CMAKE_EXPORT_COMPILE_COMMANDS ON )
 
+set( CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin )
+set( CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib )
+set( CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib )
+
 ProcessorCount( NPROC )
 
 #############################################################################
