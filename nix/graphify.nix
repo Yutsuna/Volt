699e989ddca231fae3ec6d454026e8a1043ed20c
Yutsuna <leo.quinzler@epitech.eu>
2026-08-03 20:27:22 +0200

    Fix(Nix): Graphify update because EVERY UPDATE IT BREAKS something

--- nix/graphify.nix
+++ nix/graphify.nix
@@ -6,12 +6,13 @@ let
     pkg:
     (pkg.overridePythonAttrs or (_: pkg)) (_: {
       dontCheckPythonMetadata = true;
+      doCheck = false;
     });
 in
 with pkgs;
 graphify.overridePythonAttrs (
   old:
-  lib.genAttrs [ "dependencies" "propagateBuildInputs" ] (
+  lib.genAttrs [ "dependencies" "propagatedBuildInputs" ] (
     key: map overrideMetadata (old.${key} or [ ])
   )
 )
