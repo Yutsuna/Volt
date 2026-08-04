f321e3415f4ef342bb682311ea046175667a469d
Yutsuna <leo.quinzler@epitech.eu>
2026-08-04 10:05:13 +0200

    Fix(Nix): Graphify install now with python uv venv

--- nix/shell.nix
+++ nix/shell.nix
@@ -5,7 +5,6 @@
 }:
 
 let
-  gccAttrs = pkgs.gcc16;
   deps = import ./deps.nix { inherit pkgs; };
 in
 pkgs.mkShell {
@@ -14,17 +13,26 @@ pkgs.mkShell {
     deps.nativeBuildInputs
     ++ deps.buildInputs
     ++ (with pkgs; [
-      gccAttrs
+      gcc16
       valgrind
       gdb
       gtest
       clang-tools
       jq
+      uv
     ]);
 
   shellHook = ''
     export CC="gcc"
     export CXX="g++"
     export NIX_CFLAGS_LINK="-fuse-ld=mold"
+
+    if [ ! -d ".venv" ]; then
+      echo ">> initializing virtual environment"
+      uv venv .venv
+      uv pip install --python .venv/bin/python graphifyy
+    fi
+
+    source .venv/bin/activate
   '';
 }
